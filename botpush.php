1593bbb4290ef49ffc542f2f8fd0b73ad4d785b3
Channarong243 <47410471+Channarong243@users.noreply.github.com>
2019-02-08 00:55:27 +0700

    Update botpush.php

--- botpush.php
+++ botpush.php
@@ -9,7 +9,7 @@ $access_token = 'P/sgtVp2vonaCA91dELVzY/HC9/IQFMSLu7a94/qcQB0muF5JvYy5QY4FMiwMTT
 
 $channelSecret = '46bbb4575cb14105257dcbe34f57c5a3';
 
-$pushID = 's8tm4prcnBI18M1hvaTAw5JacBmMuVDRfuGEwhWbd80';
+$pushID = 'U9f6c101adc821c4fee95c3cacc9e478b';
 
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
